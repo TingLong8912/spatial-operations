446f15e7afaa898f6fa98eb0b43b4183289e236f
Nolonger2000 <b08208047@g.ntu.edu.tw>
2024-12-03 15:19:23 +0800

    return output format

--- image/src/routes/ApiRoutes.mjs
+++ image/src/routes/ApiRoutes.mjs
@@ -593,21 +593,11 @@ const getBinaryDistanceObjectProbability = (inputPt, referObjectDict, referColum
 
             //output result
             if (tempResult.length === 2) {
-                resultDistanceMiddle[key] = tempResult.map(name => {
-                    return {
-                        name: name,
-                        feature: referObjectDict[key].features.find(feature => feature.properties[objectNameCol] === name)
-                    };
-                });
+                resultDistanceMiddle[key] = tempResult;
                 resultDistanceNear[key] = [];
             } else if (tempResult.length === 1) {
                 resultDistanceMiddle[key] = [];
-                resultDistanceNear[key] = tempResult.map(name => {
-                    return {
-                        name: name,
-                        feature: referObjectDict[key].features.find(feature => feature.properties[objectNameCol] === name)
-                    };
-                });
+                resultDistanceNear[key] = tempResult;
             } else {
                 resultDistanceMiddle[key] = [];
                 resultDistanceNear[key] = [];
